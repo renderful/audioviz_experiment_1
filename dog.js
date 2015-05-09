412e24099db7c9f739ebbaaab8e1599432251c3f
Codey Christensen <codey@renderf.nl>
2015-05-08 22:40:59 -0600

    3 heads are better than 1

--- dog.js
+++ dog.js
@@ -6,35 +6,47 @@ document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
 
 var dogTexture = PIXI.Texture.fromImage("thatboy.png");
-var dog = new PIXI.Sprite(dogTexture);
 
-dog.position.x = window.innerWidth/2;
-dog.position.y = window.innerHeight/2;
+var dogs = [];
+var number_of_dogs = 3;
 
-dog.scale.x = 2;
-dog.scale.y = 2;
-dog.anchor.x = 0.5;
-dog.anchor.y = 0.5;
+// create number_of_dogs sprites
+for(i = 0; i < number_of_dogs; i++) {
+    var dog = new PIXI.Sprite(dogTexture);
 
-// create a filter
-var bloomFilter = new PIXI.filters.BloomFilter();
-bloomFilter.blur = 100;
-var pixelateFilter = new PIXI.filters.PixelateFilter();
-var twistFilter = new PIXI.filters.TwistFilter();
-twistFilter.angle = 2;
-twistFilter.offset.x = 0.3;
-twistFilter.offset.y = 0.6;
- 
-// set the filter
-stage.filters = [bloomFilter, twistFilter];
-// stage.filters = [bloomFilter];
+    dog.position.x = window.innerWidth/2 * (i+1)/2;
+    dog.position.y = window.innerHeight/2;
 
-stage.addChild(dog);
-var i = 0;
+    dog.scale.x = 2;
+    dog.scale.y = 2;
+    dog.anchor.x = 0.5;
+    dog.anchor.y = 0.5;
+
+    // create a filter
+    var bloomFilter = new PIXI.filters.BloomFilter();
+    bloomFilter.blur = 100;
+    var pixelateFilter = new PIXI.filters.PixelateFilter();
+    var twistFilter = new PIXI.filters.TwistFilter();
+    twistFilter.angle = 2;
+    twistFilter.offset.x = 0.3;
+    twistFilter.offset.y = 0.6;
+     
+    // set the filter
+    dog.filters = [bloomFilter, twistFilter];
+    // stage.filters = [bloomFilter];
+
+    dogs.push(dog);
+
+    stage.addChild(dog);
+}
+
+var r = 0;
 
 function animate() {
-    dog.rotation += 0.01;
-    i += 0.01;
+    dogs[0].rotation += 0.01;
+    dogs[1].rotation -= 0.01;
+    dogs[2].rotation += 0.002;
+    r += 0.01;
 
     renderer.render(stage);
 
