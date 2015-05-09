c14b98451091fa9f6072ea0cf323d35319bbbb5c
Codey Christensen <codey@renderf.nl>
2015-05-08 22:52:44 -0600

    pixelate at high volume and move filters out of loop

--- dog.js
+++ dog.js
@@ -10,9 +10,21 @@ var dogTexture = PIXI.Texture.fromImage("thatboy.png");
 var dogs = [];
 var number_of_dogs = 3;
 
+var bloomFilter = new PIXI.filters.BloomFilter();
+bloomFilter.blur = 100;
+var pixelateFilter = new PIXI.filters.PixelateFilter();
+var twistFilter = new PIXI.filters.TwistFilter();
+twistFilter.angle = 2;
+twistFilter.offset.x = 0.3;
+twistFilter.offset.y = 0.6;
+
 // create number_of_dogs sprites
 for(i = 0; i < number_of_dogs; i++) {
     var dog = new PIXI.Sprite(dogTexture);
+
+    // if (i == 1) {
+        dog.blendMode = PIXI.BLEND_MODES.ADD
+    // }
 
     dog.position.x = window.innerWidth/2 * (i+1)/2;
     dog.position.y = window.innerHeight/2;
@@ -22,14 +34,6 @@ for(i = 0; i < number_of_dogs; i++) {
     dog.anchor.x = 0.5;
     dog.anchor.y = 0.5;
 
-    // create a filter
-    var bloomFilter = new PIXI.filters.BloomFilter();
-    bloomFilter.blur = 100;
-    var pixelateFilter = new PIXI.filters.PixelateFilter();
-    var twistFilter = new PIXI.filters.TwistFilter();
-    twistFilter.angle = 2;
-    twistFilter.offset.x = 0.3;
-    twistFilter.offset.y = 0.6;
      
     // set the filter
     dog.filters = [bloomFilter, twistFilter];
@@ -52,7 +56,10 @@ function animate() {
 
     requestAnimationFrame(animate);
 
-    console.log(meter.volume);
+    if(meter.volume > 0.3) {
+        stage.filters = [pixelateFilter];
+    }
+
     twistFilter.angle = meter.volume * 15;
     // twistFilter.angle = Math.abs(Math.sin(i));
     // twistFilter.angle = Math.sin(i);
